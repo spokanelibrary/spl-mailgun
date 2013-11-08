d2ea948871ce2ac7f8c9d6b5cbb1423d667c6435
Sean Girard <seangirard@yahoo.com>
2013-11-08 12:46:22 -0800

    add plugin

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package SPL_Mailgun
+ * @version 0.1
+ */
+
+/*
+Plugin Name: SPL Mailgun
+Plugin URI: http://www.spokanelibrary.org/
+Description: Hooks a custom post type to a mailgun campaign.
+Author: Sean Girard
+Author URI: http://seangirard.com
+Version: 0.1
+*/
+
+
+?>
