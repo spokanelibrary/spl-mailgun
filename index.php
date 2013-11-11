bdfbf7d48746ee3df73ff10b774d23f35255e09a
Sean Girard <seangirard@yahoo.com>
2013-11-11 10:47:43 -0800

    metabox

--- index.php
+++ index.php
@@ -127,8 +127,8 @@ function spl_mailgun_newsletter_list_meta_box($object, $box) { ?>
 
 
 <?php
-add_action( 'init', 'be_initialize_cmb_meta_boxes', 9999 );
-function be_initialize_cmb_meta_boxes() {
+add_action( 'init', 'spl_initialize_cmb_meta_boxes', 9999 );
+function spl_initialize_cmb_meta_boxes() {
   if ( !class_exists( 'cmb_Meta_Box' ) ) {
     require_once( get_plugin_directory() . '/metabox/init.php' );
   }
