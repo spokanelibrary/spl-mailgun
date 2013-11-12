aae4949a0b9ef120689d1accbd20e95a70523f90
Sean Girard <seangirard@yahoo.com>
2013-11-12 11:51:39 -0800

    single

--- functions.php
+++ functions.php
@@ -27,7 +27,7 @@ function spl_news_post_select($post, $id) {
 													); 
 
 	if ( !empty($select) ) {
-		$attach = get_post($post->ID);
+		$attach = get_post($select);
 	
 		echo '<h4>'.$attach->post_title.'</h4>';
 		echo wpautop($attach->post_content);
