f229896e52d931ff00cc9b4c0586be76b862106b
Sean Girard <seangirard@yahoo.com>
2013-11-12 10:50:40 -0800

    single

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -5,7 +5,7 @@ This is a custom template!
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h1 class=""><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
