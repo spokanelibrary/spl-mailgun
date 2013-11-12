62321e61badaf881672b99072094d82cbe79ee90
Sean Girard <seangirard@yahoo.com>
2013-11-12 11:20:18 -0800

    single

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -1,30 +1,27 @@
 <?php //get_template_part('templates/content', 'single'); ?>
 
 <?php while (have_posts()) : the_post(); ?>
+	<div class="page-header">
+    <h1><?php the_title(); ?></h1>
+    <?php// get_template_part('templates/entry-meta'); ?>
+  </div>
 
-  <article <?php post_class(); ?>>
-    <header class="page-header">
-      <h1><?php the_title(); ?></h1>
-      <?php// get_template_part('templates/entry-meta'); ?>
-    </header>
-
-    <div class="row-fluid">
-	    <div class="col-sm-7">
-	      <?php the_content(); ?>
+  <div class="row-fluid">
+    <div class="col-sm-7">
+      <?php the_content(); ?>
+    </div>
+    <div class="col-sm-5">
+    	<h4><?php spl_news_sidebar_headline($post); ?></h4>
+    	<div class='well well-sm'>
+  	  	<?php spl_news_sidebar_content($post); ?>
 	    </div>
-	    <div class="col-sm-5">
-	    	<h4><?php spl_news_sidebar_headline($post); ?></h4>
-	    	<div class='well well-sm'>
-	  	  	<?php spl_news_sidebar_content($post); ?>
-		    </div>
-	    </div>
-	  </div>
+    </div>
+  </div>
 
 
 
-    <footer>
+  <footer>
 
-    </footer>
-    <?php //comments_template('/templates/comments.php'); ?>
-  </article>
+  </footer>
+  <?php //comments_template('/templates/comments.php'); ?>
 <?php endwhile; ?>
