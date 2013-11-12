caf29d3d4fc1799df292d44f765dac5fed62d00a
Sean Girard <seangirard@yahoo.com>
2013-11-12 11:22:50 -0800

    single

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -11,8 +11,11 @@
       <?php the_content(); ?>
     </div>
     <div class="col-sm-5">
-    	<h4><?php spl_news_sidebar_headline($post); ?></h4>
+    	
     	<div class='panel panel-default' style="border-left-width:5px;">
+    		<div class="panel-heading">
+    			<h4><?php spl_news_sidebar_headline($post); ?></h4>
+    		</div>
     		<div class="panel-body">
   	  	<?php spl_news_sidebar_content($post); ?>
 	  	  </div>
