fabf7163ddc0dac1e15c7d1a198fda438834e63d
Sean Girard <seangirard@yahoo.com>
2013-11-12 10:50:13 -0800

    template

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -1,3 +1,19 @@
-<?php get_template_part('templates/content', 'single'); ?>
+<?php //get_template_part('templates/content', 'single'); ?>
 
 This is a custom template!
+
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer>
+    <?php comments_template('/templates/comments.php'); ?>
+  </article>
+<?php endwhile; ?>
