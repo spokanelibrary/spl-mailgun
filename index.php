b631f180ceacb952d86a708cc68133b9c3f9648d
Sean Girard <seangirard@yahoo.com>
2013-11-12 10:09:14 -0800

    templates

--- index.php
+++ index.php
@@ -49,8 +49,8 @@ class SPL_Mailgun_Newsletter {
 		global $wp_query, $post;
 		/* Checks for single template by post type */
 		if ($post->post_type == "newsletter"){
-				echo 'test!';
-		    if(file_exists(plugin_basename('/templates/single.php')))
+				if(file_exists(plugin_basename('/templates/single.php')))
+		    	echo 'test!';
 		        return plugin_basename('/templates/single.php');
 		}
 		    return $single;
