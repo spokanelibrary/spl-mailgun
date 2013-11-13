05bd80e06b0ba4bccdec58b2d49b809c71183f70
Sean Girard <seangirard@yahoo.com>
2013-11-13 15:26:33 -0800

    settings

--- settings.php
+++ settings.php
@@ -35,6 +35,86 @@ class SPL_Mailgun_Newsletter_Settings {
     echo '</div>';    
   }
   
+  public function page_init()
+  {        
+      register_setting(
+          'my_option_group', // Option group
+          'my_option_name', // Option name
+          array( $this, 'sanitize' ) // Sanitize
+      );
+
+      add_settings_section(
+          'setting_section_id', // ID
+          'My Custom Settings', // Title
+          array( $this, 'print_section_info' ), // Callback
+          'spl-mailgun-newsletter-settings' // Page
+      );  
+
+      add_settings_field(
+          'id_number', // ID
+          'ID Number', // Title 
+          array( $this, 'id_number_callback' ), // Callback
+          'spl-mailgun-newsletter-settings', // Page
+          'setting_section_id' // Section           
+      );      
+
+      add_settings_field(
+          'title', 
+          'Title', 
+          array( $this, 'title_callback' ), 
+          'spl-mailgun-newsletter-settings', 
+          'setting_section_id'
+      );      
+  }
+
+  /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['title'] ) )
+            $new_input['title'] = sanitize_text_field( $input['title'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function id_number_callback()
+    {
+        printf(
+            '<input type="text" id="id_number" name="my_option_name[id_number]" value="%s" />',
+            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function title_callback()
+    {
+        printf(
+            '<input type="text" id="title" name="my_option_name[title]" value="%s" />',
+            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
+        );
+    }
+}
+
 } // SPL_Mailgun_Newsletter_Settings
 
 ?>
