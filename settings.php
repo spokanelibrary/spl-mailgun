328ba0376533247ff2ea2a4bfba013037e607ff5
Sean Girard <seangirard@yahoo.com>
2013-11-13 14:49:25 -0800

    settings

--- settings.php
+++ settings.php
@@ -5,10 +5,10 @@ class SPL_Mailgun_Newsletter_Settings {
 	public $options;
 
 	function __construct() {
-		//add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
     //add_action( 'admin_init', array( $this, 'page_init' ) );
 	}
-	/*
+	
 	public function add_plugin_page() {
 	  add_options_page(
 	      'Settings Admin', 
@@ -22,7 +22,7 @@ class SPL_Mailgun_Newsletter_Settings {
   public function create_admin_page() {
       
   }
-  */
+  
 
 } // SPL_Mailgun_Newsletter_Settings
 
