33bd00f9ec2411401df293ee52b753e5d51d9c47
Sean Girard <seangirard@yahoo.com>
2013-11-13 09:02:46 -0800

    img

--- index.php
+++ index.php
@@ -37,6 +37,29 @@ function spl_set_image_attributes($html, $id, $caption, $title, $align, $url, $s
 }
 */
 
+//	https://gist.github.com/mkdizajn/7352469
+//----------------------------------------------------------/
+//  responsive images [ 1) add img-responsive class 2) remove dimensions ]
+//----------------------------------------------------------/
+ 
+function bootstrap_responsive_images( $html ){
+  $classes = 'img-responsive'; // separated by spaces, e.g. 'img image-link'
+ 
+  // check if there are already classes assigned to the anchor
+  if ( preg_match('/<img.*? class="/', $html) ) {
+    $html = preg_replace('/(<img.*? class=".*?)(".*?\/>)/', '$1 ' . $classes . ' $2', $html);
+  } else {
+    $html = preg_replace('/(<img.*?)(\/>)/', '$1 class="' . $classes . '" $2', $html);
+  }
+  // remove dimensions from images,, does not need it!
+  $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+  return $html;
+}
+add_filter( 'the_content','bootstrap_responsive_images',10 );
+add_filter( 'post_thumbnail_html', 'bootstrap_responsive_images', 10 );
+
+
+
 class SPL_Mailgun_Newsletter {
 
 	var $config;
