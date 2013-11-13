414a20b72ee3cc876b62ac5619ec16c711701283
Sean Girard <seangirard@yahoo.com>
2013-11-13 14:39:29 -0800

    admin

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ require('functions.php');
 $config = new SPL_Mailgun_Newsletter_Config();
 $newsletter = new SPL_Mailgun_Newsletter($config);
 
+if ( is_admin() ) {
+	$admin = new SPL_Mailgun_Newsletter_Admin();
+}
+
 class SPL_Mailgun_Newsletter {
 
 	var $config;
