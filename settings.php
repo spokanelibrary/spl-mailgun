dd937c67ae46bf03c59cbc702f9f21fa54223271
Sean Girard <seangirard@yahoo.com>
2013-11-18 12:19:32 -0800

    settings

--- settings.php
+++ settings.php
@@ -101,7 +101,7 @@ class SPL_Mailgun_Newsletter_Settings {
         //print 'Enter your settings below:';
     }
 
-    protected function settings_field_text($args) {
+    public function settings_field_text($args) {
      		printf(
             '<input type="text" id="'.$args['id'].'" name="'.$args['option'].'['.$args['id'].']" value="%s" />',
             isset( $this->options[$args['id']] ) ? esc_attr( $this->options[$args['id']]) : ''
