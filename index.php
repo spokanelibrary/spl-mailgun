5c02213040738fe2473ed8db7a3000f67b6c02f2
Sean Girard <seangirard@yahoo.com>
2013-11-20 13:22:18 -0800

    templates

--- index.php
+++ index.php
@@ -338,7 +338,9 @@ class SPL_Mailgun_Newsletter {
 	// MAILGUN INTEGRATION
 
 	function getNewsletterHTML($id, $template) {
-		//$newsletter = get_post($id);
+		$newsletter = get_post($id);
+
+		$title = $newsletter['post_title'];
 
 		//return print_r($_POST, true);
 		$template = plugin_dir_path(__FILE__).'emails/'.$template;
