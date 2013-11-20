9f6396424e08d45fedf519ec62a0077475b105f9
Sean Girard <seangirard@yahoo.com>
2013-11-20 13:29:37 -0800

    templates

--- emails/Default.php
+++ emails/Default.php
@@ -878,7 +878,7 @@ body.outlook p {
                       <table class="twelve columns">
                         <tr>
                           <td>
-                            <h1><?php echo $myvar; ?></h1>
+                            <h1><?php echo $id; ?></h1>
                             <p class="lead">Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae.</p>
                             <p>
 
