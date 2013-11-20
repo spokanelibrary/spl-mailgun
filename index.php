11bc50c286c8c9999d6e92a289af7e79cdfd17e1
Sean Girard <seangirard@yahoo.com>
2013-11-20 14:25:36 -0800

    templates

--- index.php
+++ index.php
@@ -338,6 +338,9 @@ class SPL_Mailgun_Newsletter {
 	// MAILGUN INTEGRATION
 
 	function getNewsletterHTML($id, $template) {
+		remove_filter( 'the_content','bootstrap_responsive_images',10 );
+		remove_filter( 'post_thumbnail_html', 'bootstrap_responsive_images', 10 );
+
 		$news = get_post($id);
 
 		$title = $news->post_title;
