ae7269acdc8b1a89961a9eb1ab905ad0e169ff62
Sean Girard <seangirard@yahoo.com>
2013-11-21 14:41:49 -0800

    spaces

--- functions.php
+++ functions.php
@@ -3,51 +3,51 @@
 // output functions
 
 function spl_news_sidebar_headline($post) {
-	echo get_post_meta($post->ID
-													, '_spl_mailgun_newsletter_sidebar_headline'
-													, true 
-													);
+  echo get_post_meta($post->ID
+                    ,'_spl_mailgun_newsletter_sidebar_headline'
+                    ,true 
+                    );
 }
 
 function spl_news_sidebar_content($post) {
-	echo apply_filters('the_content', get_post_meta($post->ID
-													, '_spl_mailgun_newsletter_sidebar_content'
-													, true 
-													));
+  echo apply_filters('the_content', get_post_meta($post->ID
+                    ,'_spl_mailgun_newsletter_sidebar_content'
+                    ,true 
+                    ));
 }
 
 function spl_news_post_select($post, $id) {
-	$select = get_post_meta($post->ID
-													, '_spl_mailgun_newsletter_post_select_'.$id
-													, true 
-													);
-	$excerpt = get_post_meta($post->ID
-													, '_spl_mailgun_newsletter_post_select_excerpt_'.$id
-													, true 
-													); 
+  $select = get_post_meta($post->ID
+                          ,'_spl_mailgun_newsletter_post_select_'.$id
+                          ,true 
+                        );
+  $excerpt = get_post_meta($post->ID
+                          ,'_spl_mailgun_newsletter_post_select_excerpt_'.$id
+                          ,true 
+                          ); 
 
-	if ( !empty($select) ) {
-		$attach = get_post($select);
-		$permalink = get_permalink($select);
-		echo '<p class="lead"><a href="'.$permalink.'">'.$attach->post_title.'</a></p>';
+  if ( !empty($select) ) {
+    $attach = get_post($select);
+    $permalink = get_permalink($select);
+    echo '<p class="lead"><a href="'.$permalink.'">'.$attach->post_title.'</a></p>';
 
-		if ( !empty($excerpt) ) {
-			if (!empty($attach->post_excerpt)) { 
-				echo wpautop($attach->post_excerpt);
-			} else {
-				echo wpautop(wp_trim_words($attach->post_content, 80));
-			}
-		} else {
-			echo wpautop($attach->post_content);
-		}
-		$anchor = '
-		<p>
-		<a href="'.$permalink.'"
-				class="btn btn-success">Read More &rarr;</a>
-		</p>
-		';
-		echo $anchor;
-	}
+    if ( !empty($excerpt) ) {
+      if (!empty($attach->post_excerpt)) { 
+        echo wpautop($attach->post_excerpt);
+      } else {
+        echo wpautop(wp_trim_words($attach->post_content, 80));
+      }
+    } else {
+      echo wpautop($attach->post_content);
+    }
+    $anchor = '
+    <p>
+    <a href="'.$permalink.'"
+        class="btn btn-success">Read More &rarr;</a>
+    </p>
+    ';
+    echo $anchor;
+  }
 
 }
 
