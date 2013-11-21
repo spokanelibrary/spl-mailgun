7fb9d67f95fff646e7e4b912dc3705d1f0b908ab
Sean Girard <seangirard@yahoo.com>
2013-11-21 15:02:42 -0800

    last

--- emails/Default.php
+++ emails/Default.php
@@ -882,8 +882,19 @@ body.outlook p {
               
               <!-- content start -->
                 <table class="row">
-                  
-                  
+                  <tr>
+                    <td class="wrapper last">
+                      <table class="twelve columns">
+                        <tr>
+                          <td>
+                            <h2><?php echo $title; ?></h2>
+                          </td>
+                          <td class="expander"></td>
+                        </tr>
+                      </table> 
+                    </td>
+                  </tr>
+
                   <tr>
                     <td class="wrapper">
                       
