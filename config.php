aa287cc0da04b88d1a11b98d9dfe1250026e681b
Sean Girard <seangirard@yahoo.com>
2013-11-21 14:33:37 -0800

    spaces

--- config.php
+++ config.php
@@ -2,30 +2,30 @@
 
 class SPL_Mailgun_Newsletter_Config {
 
-	var $custom;
-	var $plugin;
+  var $custom;
+  var $plugin;
 
-	function __construct() {
-		$this->custom = $this->getCustomSettings();
-		// get settings defined in admin interface
-		$this->plugin = get_option( 'spl-mailgun-newsletter' );
-	}
+  function __construct() {
+    $this->custom = $this->getCustomSettings();
+    // get settings defined in admin interface
+    $this->plugin = get_option( 'spl-mailgun-newsletter' );
+  }
 
-	function getCustomSettings() {
-		$settings = new stdClass();
+  function getCustomSettings() {
+    $settings = new stdClass();
 
-		// ToDo: slug? ...
-		
-		$settings->post_type = 'newsletter';
+    // ToDo: slug? ...
+    
+    $settings->post_type = 'newsletter';
 
-		$mailgun = new stdClass();
-		$mailgun->api = 'https://api.mailgun.net/v2/';
-		$mailgun->user = 'api';
+    $mailgun = new stdClass();
+    $mailgun->api = 'https://api.mailgun.net/v2/';
+    $mailgun->user = 'api';
 
-		$settings->mailgun = $mailgun;
+    $settings->mailgun = $mailgun;
 
-		return $settings;
-	}
+    return $settings;
+  }
 
 }
 
