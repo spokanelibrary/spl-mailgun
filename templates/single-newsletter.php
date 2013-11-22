9cc75a3afebf5402f74e41e9e03955ee35af3752
Sean Girard <seangirard@yahoo.com>
2013-11-22 10:40:37 -0800

    callout

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -49,9 +49,11 @@
       </h3>
 
       <div class="panel panel-warning panel-callout-warning">
-        <div class="panel-body text-primary" style="font-style:italic;">
+        <div class="panel-body text-primary">
           <blockquote>
-          <?php spl_news_sidebar_callout($post); ?>
+            <div style="font-style:italic;">
+            <?php spl_news_sidebar_callout($post); ?>
+            </div>
           <small><?php spl_news_sidebar_attribution($post); ?></small>
           </blockquote>
         </div>
