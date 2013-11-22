fcdb59b192c0058a13cd41c0a8091f2ff9b6d35f
Sean Girard <seangirard@yahoo.com>
2013-11-22 15:21:55 -0800

    template

--- emails/Default.php
+++ emails/Default.php
@@ -334,12 +334,20 @@ h1, h2, h3, h4, h5, h6 {
   word-break: normal;
 }
 
+h1 {font-size: 20px;}
+h2 {font-size: 18px;}
+h3 {font-size: 16px;}
+h4 {font-size: 14px;}
+h5 {font-size: 12px;}
+h6 {font-size: 10px;}
+/*
 h1 {font-size: 40px;}
 h2 {font-size: 36px;}
 h3 {font-size: 32px;}
 h4 {font-size: 28px;}
 h5 {font-size: 24px;}
 h6 {font-size: 20px;}
+*/
 body, table.body, p {font-size: 14px;line-height:19px;}
 
 p { 
