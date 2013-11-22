30be33b8c313356a1a89ecdfc4075586ce499740
Sean Girard <seangirard@yahoo.com>
2013-11-22 13:55:20 -0800

    template

--- emails/Default.php
+++ emails/Default.php
@@ -811,9 +811,8 @@ body.outlook p {
                       <table class="four columns">
                         <tr>
                           <td>
-                            <h2><h1><?php echo $subtitle; ?></h1></h2>
-                            <p class="lead">Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae.</p>
-                            <p>Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae. consequat vel lacus. Sed iaculis pulvinar ligula, ornare fringilla ante viverra et. In hac habitasse platea dictumst. Donec vel orci mi, eu congue justo. Integer eget odio est, eget malesuada lorem. Aenean sed tellus dui, vitae viverra risus. Nullam massa sapien, pulvinar eleifend fringilla id, convallis eget nisi. Mauris a sagittis dui. Pellentesque non lacinia mi. Fusce sit amet libero sit amet erat venenatis sollicitudin vitae vel eros. Cras nunc sapien, interdum sit amet porttitor ut, congue quis urna.</p>
+                            <h2><?php echo $subtitle; ?></h2>
+                            <?php echo $content; ?>
                           </td>
                           <td class="expander"></td>
                         </tr>
