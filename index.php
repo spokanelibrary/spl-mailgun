68ec997228c752416e45adfbca2ea919bed81e17
Sean Girard <seangirard@yahoo.com>
2013-11-22 08:29:42 -0800

    inliner

--- index.php
+++ index.php
@@ -408,7 +408,7 @@ class SPL_Mailgun_Newsletter {
 
   function notifyMailgunResponse($response, $list=null, $template=null) {
     if ( !is_null($template) ) {
-      $response .= print_r($this->config->custom->inliner);
+      $response .= print_r($this->config->custom->inliner->api.'raw');
       //$response .= $this->curlProxy($this->config->custom->inliner->api.'raw';
       //$response .= $template = $this->curlProxy($this->config->custom->inliner->api.'raw', array('source', $template));
     }
