29e6434eecd62ced7f1090510bf9b00db136fc61
Sean Girard <seangirard@yahoo.com>
2013-11-26 14:49:20 -0800

    template

--- index.php
+++ index.php
@@ -395,7 +395,7 @@ class SPL_Mailgun_Newsletter {
 
     $link = get_permalink($id);
 
-    $date = the_date();
+    $date = $post->post_date;
 
     $title = $post->post_title;
     $content = str_replace(array('<figure','</figure>','<figcaption','</figcaption>')
