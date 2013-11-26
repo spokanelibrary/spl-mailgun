748373a5982c9325e391a604c9d7797bf28a4461
Sean Girard <seangirard@yahoo.com>
2013-11-26 15:35:42 -0800

    template

--- emails/Default.php
+++ emails/Default.php
@@ -760,7 +760,7 @@ body.outlook p {
                               <img src="http://beta.spokanelibrary.org/assets/img/SPL-Logo-inverse.png" style="height:80px; width:170px;">
                             </td>
                             <td class="six sub-columns last" style="text-align:right; vertical-align:middle;">
-                              <span class="template-label"><?php echo $date; ?></span>
+                              <span class="template-label serif"><?php echo $date; ?></span>
                             </td>
                             <td class="expander"></td>
                           </tr>
