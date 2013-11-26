1a43b20492831233e5a93d338de0f8badcd569c5
Sean Girard <seangirard@yahoo.com>
2013-11-26 11:38:17 -0800

    template

--- emails/Default.php
+++ emails/Default.php
@@ -817,7 +817,7 @@ body.outlook p {
                           <td class="expander"></td>
                         </tr>
                         <tr>
-                          <td class="sidebar">
+                          <td class="panel sidebar">
                             <h2><?php echo $sidebar; ?></h2>
                           </td>
                           <td class="expander"></td>
