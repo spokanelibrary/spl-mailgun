93e93d0cb305abc68e88733832f7e56b1fa113b7
Sean Girard <seangirard@yahoo.com>
2013-11-27 15:11:11 -0800

    template

--- functions.php
+++ functions.php
@@ -42,6 +42,7 @@ function spl_news_post_select($post, $id) {
 
   if ( !empty($select) ) {
     $attach = get_post($select);
+    print_r($attach);
     $permalink = get_permalink($select);
     echo '<p class="lead"><a href="'.$permalink.'">'.$attach->post_title.'</a></p>';
 
