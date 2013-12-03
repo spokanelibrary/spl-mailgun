a4fa0f470de967d791880a6574541360b37bd5cb
Sean Girard <seangirard@yahoo.com>
2013-12-03 15:21:32 -0800

    template

--- emails/Default.php
+++ emails/Default.php
@@ -64,7 +64,7 @@
                             </td>
                             <td class="six sub-columns last" style="text-align:right; vertical-align:middle;">
                               <h1 class="serif text-right color-light">News</h1>
-                              <span class="serif text-right white"><em>from your library</em></span>
+                              <span class="serif text-right color-light"><em>from your library</em></span>
                             </td>
                             <td class="expander"></td>
                           </tr>
