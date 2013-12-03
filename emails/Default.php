99761bc2754ab431a5a31213d1cc91fa34a13922
Sean Girard <seangirard@yahoo.com>
2013-12-03 15:13:12 -0800

    template

--- emails/Default.php
+++ emails/Default.php
@@ -63,7 +63,7 @@
                               <a href="http://beta.spokanelibrary.org"><img src="http://beta.spokanelibrary.org/assets/img/SPL-Logo-inverse.png" style="height:80px; width:170px;"></a>
                             </td>
                             <td class="six sub-columns last" style="text-align:right; vertical-align:middle;">
-                              <h1 class="serif text-right white">News</h1>
+                              <h1 class="serif text-right color-light">News</h1>
                               <span class="serif text-right white"><em>from your library</em></span>
                             </td>
                             <td class="expander"></td>
