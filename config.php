193d4f3169f4d132b6b267c2eed67c1592ee4df7
Sean Girard <seangirard@yahoo.com>
2013-12-04 13:50:36 -0800

    template

--- config.php
+++ config.php
@@ -19,7 +19,7 @@ class SPL_Mailgun_Newsletter_Config {
     $settings->post_type = 'newsletter';
 
     $sidebar = new stdClass();
-    $sidebar->img_width = '170px';
+    $sidebar->img_width = '175px';
 
     $settings->sidebar = $sidebar;
 
