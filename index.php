67d78a8ab0ea22af6aef73cd3097800313950474
Sean Girard <seangirard@yahoo.com>
2013-12-23 14:26:23 -0800

    flush_rewrite_rules() on activation

--- index.php
+++ index.php
@@ -38,9 +38,15 @@ class SPL_Mailgun_Newsletter {
     }
   }
 
+  function activateNewsletter() {
+    flush_rewrite_rules();
+  }
+
   function initNewsletter() {
     //$this->registerPostTemplates();
     
+    register_activation_hook( __FILE__, array( $this, activateNewsletter() ) );
+
     add_action( 'init', array( $this, 'registerPostType' ) );
     add_action( 'init', array($this, 'initCmbMetaBoxes'), 9999 );
 
