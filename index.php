a50ebe200524c740c3e6f1a11bf45479719f264d
Sean Girard <seangirard@yahoo.com>
2013-12-26 14:25:13 -0800

    thumb

--- index.php
+++ index.php
@@ -537,8 +537,8 @@ EOT;
       $post->title = $attach->post_title;
 
       // todo: featured img
-      //$post->thumbnail = get_the_post_thumbnail($attach->ID, 'medium', array('class'=>'img-responsive'));
-      $post->thumbnail = $attach->ID;
+      //$post->thumbnail = $attach->ID;
+      $post->thumbnail = get_the_post_thumbnail($attach->ID, 'medium', array('class'=>'img-responsive'));
 
       if ( !empty($excerpt) ) {
         if (!empty($attach->post_excerpt)) { 
