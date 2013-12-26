aa8539aeb60911a3eced38c0c999b8c632ec3540
Sean Girard <seangirard@yahoo.com>
2013-12-26 13:07:24 -0800

    callout

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -53,19 +53,7 @@
     </div><!-- /.col -->
     <div class="col-sm-4">
 
-      <?php if ( isset($meta->callout && $meta->callout['content']) ) :  ?>
-      <!-- sidebar callout -->
-      <div class="panel panel-warning panel-callout-warning">
-        <div class="panel-body text-primary">
-          <div style="font-style:italic;">
-          <?php echo $meta->callout['content']; ?>
-          </div>
-          <?php if ( isset($meta->callout['attrib']) ) :  ?>
-          <span class="help-block">&mdash;<?php echo $meta->callout['attrib']; ?></span>
-          <?php endif; ?>
-        </div>
-      </div>
-      <?php endif; ?>
+      
       <?php if ( isset($meta->sidebar) ) :  ?>
         <!-- sidebar -->
         <?php if ( isset($meta->sidebar['headline']) ) :  ?>
