c8d50b25019b2ba922461d61a2b7ec687ca57394
Sean Girard <seangirard@yahoo.com>
2013-12-27 15:12:31 -0800

    legal

--- emails/Aside.php
+++ emails/Aside.php
@@ -340,6 +340,8 @@
                 <?php endif; ?>
 
                 <!-- colophon -->
+                <br>
+                <br>
                 <table class="row footer">
                   <tr>
                     <td class="wrapper">
@@ -381,7 +383,26 @@
                   </tr>
                 </table>
 
-
+                <!-- legal -->
+                <table class="row">
+                  <tr>
+                    <td class="wrapper last">
+            
+                      <table class="twelve columns">
+                        <tr>
+                          <td align="center">
+                            <center>
+                              <p style="text-align:center;"><a href="#">Terms</a> | <a href="#">Privacy</a> | <a href="#">Unsubscribe</a></p>
+                            </center>
+                          </td>
+                          <td class="expander"></td>
+                        </tr>
+                      </table>
+            
+                    </td>
+                  </tr>
+                </table>
+                
 
               <!-- content start -->
               
