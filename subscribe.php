e003ded137f943d58a9d205c2e1e707f26cf83aa
Sean Girard <seangirard@yahoo.com>
2013-12-30 12:34:27 -0800

    legend

--- subscribe.php
+++ subscribe.php
@@ -1,5 +1,6 @@
-<div class="row">
 <form class="form-horizontal" role="form">
+  <fieldset>
+    <legend>Your ideas intrigue me</legend>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-4 control-label">Your Email Address</label>
     <div class="col-sm-8">
@@ -14,5 +15,5 @@
       <button type="submit" class="btn btn-default">Subscribe &rarr;</button>
     </div>
   </div>
+  </fieldset>
 </form>
-</div>
