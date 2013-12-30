119756a06964f03a391610785061e35022da483d
Sean Girard <seangirard@yahoo.com>
2013-12-30 11:45:44 -0800

    widget

--- subscribe.php
+++ subscribe.php
@@ -1,13 +1,13 @@
 <form class="form-horizontal" role="form">
   <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-    <div class="col-sm-10">
+    <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
+    <div class="col-sm-9">
       <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Sign in</button>
+    <div class="col-sm-offset-3 col-sm-9">
+      <button type="submit" class="btn btn-default">Subscribe</button>
     </div>
   </div>
 </form>
