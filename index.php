3e9f0d6f35bb0507977bc2f710bd606338a63ac1
Sean Girard <seangirard@yahoo.com>
2013-12-30 15:03:07 -0800

    widget

--- index.php
+++ index.php
@@ -858,7 +858,7 @@ EOT;
     $result = curl_exec($ch);
 
     // grab http response code
-    $result->response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $result = curl_getinfo($ch, CURLINFO_HTTP_CODE);
      
     // close cURL resource, and free up system resources
     curl_close($ch);
