c44de73e3c9777b17c7320fa3bdfce448cd224f2
Sean Girard <seangirard@yahoo.com>
2013-12-30 12:48:24 -0800

    widget

--- subscribe.php
+++ subscribe.php
@@ -1,19 +1,19 @@
 <form class="form-horizontal" role="form">
   <fieldset>
-    <legend>Subscribe</legend>
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-4 control-label">Your Email Address</label>
-    <div class="col-sm-8">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email Address">
-      <span class="help-block">
-        This is a help block
-      </span>
+    <legend class="text-muted">Subscribe</legend>
+    <div class="form-group">
+      <label for="inputEmail3" class="col-sm-4 control-label">Your Email Address</label>
+      <div class="col-sm-8">
+        <input type="email" class="form-control" id="inputEmail3" placeholder="Email Address">
+        <span class="help-block">
+          This is a help block
+        </span>
+      </div>
     </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-4 col-sm-8">
-      <button type="submit" class="btn btn-default">Subscribe &rarr;</button>
+    <div class="form-group">
+      <div class="col-sm-offset-4 col-sm-8">
+        <button type="submit" class="btn btn-default">Subscribe &rarr;</button>
+      </div>
     </div>
-  </div>
   </fieldset>
 </form>
