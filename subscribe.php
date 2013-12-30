b5ca1c96b671a21e51ef469f6842194cc338895d
Sean Girard <seangirard@yahoo.com>
2013-12-30 12:52:10 -0800

    widget

--- subscribe.php
+++ subscribe.php
@@ -2,7 +2,10 @@
   <fieldset>
     <legend class="text-muted">Subscribe</legend>
     <div class="form-group">
-      <label for="inputEmail3" class="col-sm-4 control-label">Your Email Address</label>
+      <label for="inputEmail3" class="col-sm-4 control-label">
+        Your Email Address 
+        <small class="glyphicon glyphicon-envelope"></small>
+      </label>
       <div class="col-sm-8">
         <input type="email" class="form-control" id="inputEmail3" placeholder="Email Address">
         <span class="help-block">
@@ -12,7 +15,10 @@
     </div>
     <div class="form-group">
       <div class="col-sm-offset-4 col-sm-8">
-        <button type="submit" class="btn btn-default">Subscribe &rarr;</button>
+        <button type="submit" class="btn btn-primary">
+          <small class="glyphicon glyphicon-inbox"></small>
+          Subscribe &rarr;
+        </button>
       </div>
     </div>
   </fieldset>
