cde662aa35fb03a23dce278e6096134df2ebdc59
Sean Girard <seangirard@yahoo.com>
2013-12-31 13:22:16 -0800

    widgets

--- widgets/subscribe-response.php
+++ widgets/subscribe-response.php
@@ -2,16 +2,20 @@
 
 <div class="alert alert-success">
   <h3>Thank You!</h3>
+  <p>
+    We have added you to our distribution list.
+  </p>
 </div>
 
 <?php else: ?>
+
 <div class="alert alert-warning">
   <h3>Whoops!</h3>
   <p>
     Something went wrong with your subscription:
   </p>
   <blockquote>
-    <?php echo $vars->result->message; ?>
+    <em><?php echo $vars->result->message; ?></em>
   </blockquote>
   <p>
     <a href="<?php echo $slug->subscribe; ?>" class="btn btn-warning">
@@ -20,14 +24,4 @@
   </p>
 </div>
 
-<pre>
-<?php print_r($vars) ?>
-</pre>  
-
 <?php endif; ?>
-
-
-
-<a href="<?php echo $slug->subscribe; ?>">Subscribe</a>
-<br>
-<a href="<?php echo $slug->unsubscribe; ?>">Unsubscribe</a>
