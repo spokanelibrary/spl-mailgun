55afad44b33c4fc5bd5a5bb0285f687361902ffd
Sean Girard <seangirard@yahoo.com>
2013-12-31 10:24:25 -0800

    js

--- index.php
+++ index.php
@@ -48,13 +48,13 @@ class SPL_Mailgun_Newsletter {
 
   function widgetSubscribe() {
 
-    wp_enqueue_script( 'spl-mailgun', plugins_url( 'js/spl-mailgun.js', __FILE__ ), false );
+    wp_enqueue_script( 'spl-mailgun', plugins_url( 'js/spl-mailgun.js', __FILE__ ), false, false );
 
     if ( !empty($_REQUEST['spl-subscribe']) ) {
       //$result = $this->addAddressToMailingList('seangirard@yahoo.com', 'news@spokanelibrary.mailgun.org');
       //$result = $this->removeAddressFromMailingList('seangirard@yahoo.com', 'news@spokanelibrary.mailgun.org');
       //$result =  $_REQUEST['spl-subscribe']; 
-      
+
     }
 
     //echo 'Subscribe Widget';
