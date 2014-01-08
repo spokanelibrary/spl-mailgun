1a884cea2805ebfd8d0f85216c52b709aa7126aa
Sean Girard <seangirard@yahoo.com>
2014-01-08 12:19:49 -0800

    current

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ class SPL_Mailgun_Newsletter {
     $q = new WP_Query( 'post_type=newsletter&posts_per_page=1' );
     //return '<pre>'.print_r($q->post, true).'</pre>';
     $slug = $q->post->post_name;
-    $page = get_page_by_path($slug);
+    $page = get_page_by_path($slug, OBJECT, 'newsletter');
     $content = apply_filters('the_content', $page->post_content);
 
     return $content;
