d011786c09d98171f18319b006a0153288e27e6e
Sean Girard <seangirard@yahoo.com>
2014-01-09 11:21:25 -0800

    tmpl

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -146,11 +146,11 @@
   	</div><!-- /.panel-body -->
 
     <div class="panel-footer">
-      <p class="text-center">
+      <span class="text-center">
         <a href="/subscribe">Subscribe</a>
         |
         <a href="/unsubscribe">Unsubscribe</a>
-      </p>
+      </span>
     </div><!-- /.panel-footer -->
 
 	</div><!-- /.panel -->
