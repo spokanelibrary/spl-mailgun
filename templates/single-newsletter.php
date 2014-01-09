58624c4e37e5b371b8769d47870143defaa9a188
Sean Girard <seangirard@yahoo.com>
2014-01-09 10:57:29 -0800

    tmpl

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -1,6 +1,6 @@
 <?php //get_template_part('templates/content', 'single'); ?>
 
-<?php get_template_part('templates/header', 'single'); ?>
+<?php get_template_part('templates/page', 'header'); ?>
 
 <div class="spl-news" style="margin-top: 0px;">
 <?php while (have_posts()) : the_post(); ?>
