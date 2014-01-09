a27bd1e6d364840d47d65ea62b4db6c2b6276612
Sean Girard <seangirard@yahoo.com>
2014-01-09 11:04:20 -0800

    tmpl

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -58,16 +58,15 @@
           <?php if ( isset($meta->callout) && isset($meta->callout['content']) ) :  ?>
           <!-- sidebar callout -->
           <div class="pull-right" style="width:240px; margin-top:40px;">
-            <div class="panel panel-warning">
-              <div class="panel-body text-primary">
-                <div style="font-style:italic;">
+            <div class="alert alert-warning">
+              <div class="text-primary" style="font-style:italic;">
                 <?php echo $meta->callout['content']; ?>
                 </div>
                 <?php if ( isset($meta->callout['attrib']) ) :  ?>
                 <span class="help-block">&mdash;<?php echo $meta->callout['attrib']; ?></span>
                 <?php endif; ?>
               </div>
-            </div>
+            
           </div>
           <?php endif; ?>
 
