0230ebbb734fda704eb03af2ae40bf010c84a471
Sean Girard <seangirard@yahoo.com>
2014-01-10 14:22:15 -0800

    meta

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -60,7 +60,12 @@
           <?php the_content(); ?>
           </article>
 
-          <?php get_template_part('templates/entry-meta'); ?>
+          <p class="byline author vcard text-muted">
+          <?php echo __('&mdash;', 'roots'); ?><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><strong><?php echo get_the_author(); ?></strong></a>
+          <!--
+          <time class="published pull-right" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+          -->
+          </p>
 
           <p class="lead text-center text-muted">
             &hellip;
