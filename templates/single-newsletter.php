1fc04d126ed996647fe4098bf9a5424cc18458b9
Sean Girard <seangirard@yahoo.com>
2014-01-10 14:20:29 -0800

    tmpl

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -60,6 +60,8 @@
           <?php the_content(); ?>
           </article>
 
+          <?php echo get_the_author(); ?>
+
           <p class="lead text-center text-muted">
             &hellip;
             <i class="glyphicon glyphicon-leaf" style="padding: 0 8px 0 12px;"></i>
