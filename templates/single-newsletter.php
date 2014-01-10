ced9962abb8f0e7e42f886d98b42819dfcf4da1f
Sean Girard <seangirard@yahoo.com>
2014-01-10 13:04:41 -0800

    tmpl

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -50,7 +50,7 @@
 
       <div class="row">
         <div class="col-sm-8">
-          <h2 class="page-header text-success serif">
+          <h2 class="page-header text-success serif" style="margin-bottom:0;">
             <?php the_title(); ?>
           </h2>
           <hr>
