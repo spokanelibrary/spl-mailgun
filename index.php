9c8ba94d377d3e4e2105b5363bb13c8c457f55ae
Sean Girard <seangirard@yahoo.com>
2014-01-13 12:34:50 -0800

    replace

--- index.php
+++ index.php
@@ -808,7 +808,7 @@ EOT;
     $content = str_replace(array('<figure','</figure>','<figcaption','</figcaption>')
                           ,array('<div', '</div>', '<div', '</div>')
                           ,apply_filters('the_content', $post->post_content));
-
+    $content = 'testing figcaption.';
     $subtitle = get_post_meta($id
                           , '_spl_mailgun_newsletter_sidebar_headline'
                           , true 
