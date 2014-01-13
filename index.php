2f00a2f2dbba2a605e966b887a39be4075cea435
Sean Girard <seangirard@yahoo.com>
2014-01-13 09:26:42 -0800

    sidebar

--- index.php
+++ index.php
@@ -678,7 +678,10 @@ class SPL_Mailgun_Newsletter {
   // MAILGUN INTEGRATION
 
   function filterNewsletterImages( $html, $sidebar=null ){
-    $classes = 'img-responsive img-rounded center'; // separated by spaces, e.g. 'img image-link'
+    $classes = 'img-responsive img-rounded'; // separated by spaces, e.g. 'img image-link'
+    if ( !empty($sidebar) ) {
+      $classes = $classes . ' center';
+    }
 
     // check if there are already classes assigned to the anchor
     if ( preg_match('/<img.*? class="/', $html) ) {
