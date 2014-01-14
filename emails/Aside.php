02d55fa41a17c76ac929306dfe0065de71a74345
Sean Girard <seangirard@yahoo.com>
2014-01-14 11:31:39 -0800

    tmp

--- emails/Aside.php
+++ emails/Aside.php
@@ -143,7 +143,7 @@
                 <!-- message body -->
                 <table class="row">
                   <tr>
-                    <td class="wrapper" style="margin-top:0; padding-top:0;">
+                    <td class="wrapper" style="padding-top:0;">
                       <table class="eight columns">
                         <tr>
                           <td>
