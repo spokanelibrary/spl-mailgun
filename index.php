676111126d6e06437f85a6282cc902094d514fb1
Sean Girard <seangirard@yahoo.com>
2014-01-14 10:50:41 -0800

    tmpl

--- index.php
+++ index.php
@@ -735,9 +735,11 @@ class SPL_Mailgun_Newsletter {
       $html .= '<tr>';
       $html .= '<td>';
       $html .= $post->content;
+      /*
       if ( !$post->excerpt ) {
         $html .= '<h4 class="center"><center>...</center></h4>';
       }
+      */
       $html .= '</td>';
       $html .= '<td class="expander"></td>';
       $html .= '</tr>';
