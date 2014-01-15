ea4a082309e1ce7cca5f5f4c22bb4aacac282902
Sean Girard <seangirard@yahoo.com>
2014-01-15 15:11:09 -0800

    tmpl

--- emails/Aside.php
+++ emails/Aside.php
@@ -38,7 +38,7 @@
                             <td class="small center">
                               
                               <center>
-                                <strong class="muted">Online:</strong>
+                                <strong class="muted">Read&nbsp;Online:</strong>
                                 <a href="<?php echo $link; ?>"><strong><?php echo home_url() ?></strong></a>
                               </center>
 
