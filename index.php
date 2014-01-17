d47dc7af609e7a091f9c8487140f20642fd9eb02
Sean Girard <seangirard@yahoo.com>
2014-01-17 13:31:18 -0800

    sidebar

--- index.php
+++ index.php
@@ -1016,11 +1016,6 @@ class SPL_Mailgun_Newsletter {
       $posts[$i] = $this->getPostSelect($id, $i);
     }
 
-    $sb_posts = array();
-    for ( $i=1; $i<= 12; $i++ ) {
-      $sb_posts[$i] = $this->getPostSelect($id, $i, true);
-    }
-
     add_filter( 'the_content', array($this,'filterNewsletterSidebarImages'),10 );
     add_filter( 'post_thumbnail_html', array($this, 'filterNewsletterSidebarImages'), 10 );
     
@@ -1032,7 +1027,10 @@ class SPL_Mailgun_Newsletter {
                           ,array('<div', '</div>', '<div', '</div>')
                           ,apply_filters('the_content', $sidebar));
     
-    
+    $sb_posts = array();
+    for ( $i=1; $i<= 12; $i++ ) {
+      $sb_posts[$i] = $this->getPostSelect($id, $i, true);
+    }
 
     
     // these really need to be removed after being run
