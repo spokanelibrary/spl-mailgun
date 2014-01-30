e3d5588caddb5460a31fbd5c7fa34371227d5f6a
Sean Girard <seangirard@yahoo.com>
2014-01-30 12:23:07 -0800

    tmpl

--- emails/Aside.php
+++ emails/Aside.php
@@ -192,20 +192,6 @@
 
 
 
-                            <div id="attachment_157" 
-                                class="" 
-                                style="float:right;">
-                                <img class="" alt="Marcia_Michele" src="http://news.spokanelibrary.org/wordpress/media/Marcia_Michele.jpg" 
-                                width="160" height="143"
-                                style="" />
-                                <br />
-                                <div class="" 
-                                style="">Michele who used to be at Shadle (left) and Marcia who was at Indian Trail (right) – both now Downtown</div>
-                            </div>
-
-
-
-
                             <?php echo $content; ?>
                             <h4 class="center">
                               <center>...</center>
