55dba63325aa2bcef3c2dbd5451499f73a90d348
Sean Girard <seangirard@yahoo.com>
2014-01-30 13:44:14 -0800

    caption

--- emails/Aside.php
+++ emails/Aside.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width"/>
 	<style>
-  <?php include(plugin_dir_path(__FILE__).'common/ink.css'); ?>
+  <?php //include(plugin_dir_path(__FILE__).'common/ink.css'); ?>
   </style>
   <style>
   <?php include(plugin_dir_path(__FILE__).'common/wordpress.css'); ?>
