791e28b1b35fbb332b512f70e0fe6b17b8594eb1
Sean Girard <seangirard@yahoo.com>
2014-01-30 10:22:15 -0800

    sidebar

--- emails/Aside.php
+++ emails/Aside.php
@@ -248,7 +248,7 @@
                         </tr>
                         <?php endif; ?>
                         <tr>
-                          <td class="sidebar subtitle">
+                          <td class="sidebar subtitle" style="background:red;">
                             <h2 class="subtitle"><?php echo $subtitle; ?></h2>
                           </td>
                           <td class="expander"></td>
