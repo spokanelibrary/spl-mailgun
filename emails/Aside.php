1b3b2ebbc2fc91045fd4c080b739a21471ef6435
Sean Girard <seangirard@yahoo.com>
2014-02-04 14:33:29 -0800

    tmpl

--- emails/Aside.php
+++ emails/Aside.php
@@ -543,9 +543,6 @@
                                 | 
                                 <a href="http://news.spokanelibrary.org/unsubscribe/?email=%recipient_email%&list=<?php echo $list; ?>">Unsubscribe</a>
                               </p>
-                              <p>
-                                <a href="%mailing_list_unsubscribe_url%">auto unsubscribe</a>
-                              </p>
                             </center>
                           </td>
                           <td class="expander"></td>
