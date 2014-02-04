2b4e0182cc566ce0d8512153b4966bc1d5e7f5b4
Sean Girard <seangirard@yahoo.com>
2014-02-04 08:55:57 -0800

    unsub

--- emails/Aside.php
+++ emails/Aside.php
@@ -546,8 +546,8 @@
                               <p>
                                 <a href="%mailing_list_unsubscribe_url%">auto unsubscribe</a>
                                 %recipient.my_message_id%
-                                %recipient.my-custom-data.my_message_id%
-                                %recipient.my-custom-data%
+                                %my_message_id%
+                                %my-custom-data.my_message_id%
                               </p>
                             </center>
                           </td>
