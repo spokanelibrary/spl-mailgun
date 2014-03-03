fda9a648b56a6c18d7da4211c31b1aedfb04d27d
Sean Girard <seangirard@yahoo.com>
2014-03-03 08:02:44 -0800

    forgot ;

--- index.php
+++ index.php
@@ -544,7 +544,7 @@ class SPL_Mailgun_Newsletter {
     
     wp_nonce_field( basename( __FILE__ ), 'spl_mailgun_newsletter_send_nonce' );
 
-    echo 'Reminder: Max image width for primary column is 386px. For sidebar, 192px.'
+    echo 'Reminder: Max image width for primary column is 386px. For sidebar, 192px.';
 
     $subject = '
     <p>
