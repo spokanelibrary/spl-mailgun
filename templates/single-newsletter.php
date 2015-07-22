fd7c79e77be48ea93e00381ccfaf14579f3a20e3
Sean Girard <seangirard@yahoo.com>
2015-07-22 10:14:41 -0700

    news

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -99,14 +99,14 @@
 
         </div><!-- /.col -->
         <div class="col-md-4">
-          
-          <p style="text-align: center;">
-            <a href="https://epay.spokanelibrary.org/eCommerceWebModule/Home"><img class="aligncenter img-responsive" alt="Pay Fines" src="http://news.spokanelibrary.org/wordpress/media/Pay_fines-160x120.jpg" ></a>
-          </p>
 
           <p>
             <a class="btn btn-success btn-block" href="/subscribe/"><i class="glyphicon glyphicon-envelope"></i>
 Subscribe to Libray News &rarr;</a>
+          </p>
+                    
+          <p style="text-align: center;">
+            <a href="https://epay.spokanelibrary.org/eCommerceWebModule/Home"><img class="aligncenter img-responsive" alt="Pay Fines" src="http://news.spokanelibrary.org/wordpress/media/Pay_fines-160x120.jpg" ></a>
           </p>
 
           <?php if ( isset($meta->callout) && isset($meta->callout['content']) ) :  ?>
