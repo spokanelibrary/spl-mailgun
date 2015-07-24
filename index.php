ae57ed20a4fd834ff74a22aa7239f9bf441c421b
Sean Girard <seangirard@yahoo.com>
2015-07-24 12:21:02 -0700

    toc

--- index.php
+++ index.php
@@ -132,7 +132,7 @@ class SPL_Mailgun_Newsletter {
         $html .= '</h3>';
 
         $html .= '<p>';
-        $html .= $q->post->post_excerpt;
+        $html .= $q->post->ID;
         $html .= '</p>';
 
         if ( !empty($posts) ) {
