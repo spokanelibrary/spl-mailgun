181057e31f1f02708f630941a33a590afc08f823
Sean Girard <seangirard@yahoo.com>
2015-07-28 12:22:05 -0700

    news

--- index.php
+++ index.php
@@ -197,7 +197,7 @@ class SPL_Mailgun_Newsletter {
         if ( !empty($posts) ) {
           $html .= '<div class="row">';
           $html .= '<div class="col col-sm-11 col-sm-offset-1">';
-          $html .= '<h3 class="normal">';
+          $html .= '<h3 class="normal text-muted">';
           $html .= 'also in this issue:';
           $html .= '</h3>';
 
