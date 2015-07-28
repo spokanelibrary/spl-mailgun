d2d6790ea097acae654fcd0954be8e206ec0d167
Sean Girard <seangirard@yahoo.com>
2015-07-28 12:38:31 -0700

    aside

--- index.php
+++ index.php
@@ -1076,7 +1076,7 @@ class SPL_Mailgun_Newsletter {
       $html .= $post->content;
       
       if ( !$post->excerpt ) {
-        $html .= '<h4 class="center"><center>...</center></h4>';
+        //$html .= '<h4 class="center"><center>...</center></h4>';
       }
       
       $html .= '</td>';
