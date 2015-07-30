55980fa5415d4c5d6915fd9e6c370ad6bd574247
Sean Girard <seangirard@yahoo.com>
2015-07-30 09:19:07 -0700

    ellipses

--- index.php
+++ index.php
@@ -377,13 +377,13 @@ class SPL_Mailgun_Newsletter {
         $html .= '';
       }
 
-      /*
+      
       $html .= '<p class="lead text-center text-muted">
                   &hellip;
                   <i class="glyphicon glyphicon-leaf" style="padding: 0 8px 0 12px;"></i>
                   &hellip;
                 </p>';
-      */
+      
       $html .= '</aside>';
       
     }
