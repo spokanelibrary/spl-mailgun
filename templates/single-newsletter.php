f8124928c6707be738165e6fbe15f4f520774866
Sean Girard <seangirard@yahoo.com>
2015-10-27 13:52:07 -0700

    tmpl

--- templates/single-newsletter.php
+++ templates/single-newsletter.php
@@ -14,7 +14,7 @@
   */
 ?>
 
-<?php if ( 'default' != $meta->template && basename(__FILE__) != $meta->template ) : ?>
+<?php if ( !empty($meta->template) && 'default' != $meta->template && basename(__FILE__) != $meta->template ) : ?>
   <!--Digest page -->
 <?php //else: ?>
 
